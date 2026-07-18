e57ee823b8b63b734e2b64e8710fb73932f1e575
reyhana-alizada <alizadareyhana@gmail.com>
2026-07-18 19:10:30 +0430

    Create Assessment base class
    Added the Assessment class with an initializer to store assessment title and maximum score.
    This class will be used as the parent class for Quiz, Exam, and Project assessments.

--- assessment.py
+++ assessment.py
@@ -1,3 +1,8 @@
 # assessment.py
 # Parent Class: Assessment
 # Child Classes: Quiz, Exam, Project
+
+class Assessment:
+    def __init__(self, title, max_score):
+        self.title = title
+        self.max_score = max_score
