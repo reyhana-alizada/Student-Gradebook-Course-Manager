cb0e9d9e7a960c83edcc7fd37d295b3dfd662957
reyhana-alizada <alizadareyhana@gmail.com>
2026-07-19 14:21:46 +0430

    Override grade message method for Exam
    Implemented the grade message method in the Exam class.
    The method evaluates the exam result based on the calculated percentage and returns a pass or fail message.

--- assessment.py
+++ assessment.py
@@ -62,3 +62,13 @@ class Exam(Assessment):
 
     def display_info(self):
         print(f"Exam: {self.title} - Max Score: {self.max_score}")
+
+    def grade_message(self, score):
+
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 55:
+            return "Passed exam"
+
+        else:
+            return "Failed exam"
