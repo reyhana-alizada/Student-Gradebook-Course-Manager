2e4bf67cdce9146771eb871bad46eb428352ddeb
reyhana-alizada <alizadareyhana@gmail.com>
2026-07-19 14:46:21 +0430

    Initialize student and assessment lists in Course class
    I added empty lists for storing student IDs and assessment objects in the Course class.
    These lists will allow each course object to manage its related students and assessments.
    I initialized the lists during object creation to prepare them for adding data later.

--- course.py
+++ course.py
@@ -6,3 +6,9 @@ class Course:
     def __init__(self, course_code, course_name):
         self.course_code = course_code
         self.course_name = course_name
+
+        # List of student IDs
+        self.students = []
+
+        # List of Assessment Objects
+        self.assessments = []
