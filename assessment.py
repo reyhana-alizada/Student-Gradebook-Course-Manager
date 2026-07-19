0de25e8b1b87057b78938190720e7e4e34102091
reyhana-alizada <alizadareyhana@gmail.com>
2026-07-19 14:30:08 +0430

    Override grade message method for Project
    Implemented the grade message method in the Project class.
    The method evaluates project performance based on the calculated percentage and returns appropriate feedback messages.

--- assessment.py
+++ assessment.py
@@ -80,3 +80,16 @@ class Project(Assessment):
 
     def display_info(self):
         print(f"Project: {self.title} - Max Score: {self.max_score}")
+
+    def grade_message(self, score):
+
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 90:
+            return "Excellent project"
+
+        elif percentage >= 55:
+            return "Project submitted"
+
+        else:
+            return "Project needs improvement"
