2f3e2094a961be4864253a55f297d43e2da067c7
reyhana-alizada <alizadareyhana@gmail.com>
2026-07-20 17:18:51 +0430

    Add Gradebook initializer method
    Implemented the initializer method for the Gradebook class.
    Initialized dictionaries to store student objects, course objects, and student grades.
    Added the passing grade attribute to define the minimum score required to pass.
    This prepares the Gradebook object to manage students, courses, assessments, and grading data.

--- gradebook.py
+++ gradebook.py
@@ -1,2 +1,18 @@
 # gradebook.py
 # Main System Controller
+
+class Gradebook:
+
+    def __init__(self):
+        # Stores Student objects
+        self.students = {}
+
+        # Stores Course objects
+        self.courses = {}
+
+        # Stores grades
+        # {student_id: {course_code: {assessment: score}}}
+        self.grades = {}
+
+        # Passing rule
+        self.passing_grade = 55
